2e3f7fae38b3fa08d5db37ed71cf182c55bff5a2
Lucie Cupcakes <lucie_linux@protonmail.com>
2021-07-20 11:39:35 -0300

    Initial prototype

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/lucie-cupcakes/go-http-static-route
+
+go 1.16
+
+require (
+	github.com/gabriel-vasile/mimetype v1.3.1
+	gitlab.com/lucie_cupcakes/lucie-utils v0.0.0-20210716141011-b0a100cf03fb
+)
---
+++ static-route.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/gabriel-vasile/mimetype"
+	lucieutils "gitlab.com/lucie_cupcakes/lucie-utils"
+)
+
+type StaticFile struct {
+	Contents []byte
+	Path     string
+	MimeType *mimetype.MIME
+}
+
+func LoadStaticFiles(dirPath string,
+	filterFilePath func(filePath string) bool) (*map[string]*StaticFile, error) {
+	staticFiles := make(map[string]*StaticFile)
+	err := filepath.Walk(dirPath,
+		func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+			if !info.IsDir() && filterFilePath(path) {
+				fileBytes, err := lucieutils.OpenAndReadAllFile(path)
+				if err != nil {
+					return err
+				}
+				if strings.Contains(path, "/") {
+					var f StaticFile
+					f.Path = path
+					f.Contents = fileBytes
+					f.MimeType = mimetype.Detect(fileBytes)
+					path = strings.SplitN(path, "/", 2)[1]
+					staticFiles[path] = &f
+				}
+			}
+			return nil
+		})
+	if err != nil {
+		return nil, fmt.Errorf("Error walking staticRoute Filesystem: %s", err.Error())
+	}
+	return &staticFiles, nil
+}
+
+func AddStaticRoutes(staticFiles *map[string]*StaticFile) {
+	sf := *staticFiles
+	for path := range sf {
+		http.HandleFunc("/"+path, func(rw http.ResponseWriter, r *http.Request) {
+			methodOk := false
+			uri := r.URL.EscapedPath()
+			if strings.HasPrefix(uri, "/") {
+				uri = strings.TrimPrefix(uri, "/")
+				if file, ok := sf[uri]; ok {
+					rw.Header().Add("Content-Type", file.MimeType.String())
+					rw.Write(file.Contents)
+					methodOk = true
+				}
+			}
+			if !methodOk {
+				rw.WriteHeader(500)
+			}
+		})
+	}
+}
